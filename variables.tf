226de7ae95ae7be48edc42adc0040c3738fb5940
ReginaldoSantos <reginaldo_email@yahoo.com.br>
2020-09-14 01:48:12 -0300

    variables.tf: pequenos ajustes

--- variables.tf
+++ variables.tf
@@ -1,76 +1,72 @@
 variable "bucket_name" {
-  description = "Bucket name were the bastion will store the logs"
+  description = "Bucket criado para armazenar logs e chaves"
 }
 
 variable "bucket_versioning" {
   default     = true
-  description = "Enable bucket versioning or not"
+  description = "Habilita versionamento no bucket"
 }
 
 variable "bucket_force_destroy" {
   default     = false
-  description = "The bucket and all objects should be destroyed when using true"
+  description = "Força remoção de bucket e seus objetos no terraform destroy"
 }
 
 variable "tags" {
-  description = "A mapping of tags to assign"
+  description = "Mapa de tags para atribuir os resources de modo geral"
   default     = {}
   type        = map(string)
 }
 
-variable "region" {
-}
+variable "region" {}
 
 variable "cidrs" {
-  description = "List of CIDRs than can access to the bastion. Default : 0.0.0.0/0"
+  description = "Lista de CIDRs que podem acessar o bastion"
   type        = list(string)
-
-  default = [
-    "0.0.0.0/0",
-  ]
+  default = ["0.0.0.0/0"]
 }
 
 variable "is_lb_private" {
-  description = "If TRUE the load balancer scheme will be \"internal\" else \"internet-facing\""
+  description = "Se true o NLB será \"internal\" senão \"internet-facing\""
 }
 
 variable "vpc_id" {
-  description = "VPC id were we'll deploy the bastion"
+  description = "Identificador da VPC onde o bastion será construído"
 }
 
 variable "bastion_host_key_pair" {
-  description = "Select the key pair to use to launch the bastion host"
+  description = "Key pair utilizada na contrução do bastion"
 }
 
 variable "hosted_zone_id" {
-  description = "Name of the hosted zone were we'll register the bastion DNS name"
+  description = "Nome da hosted zone onde o DNS name do bastion deve ser registrado"
   default     = ""
 }
 
 variable "bastion_record_name" {
-  description = "DNS record name to use for the bastion"
+  description = "DNS record para o bastion"
   default     = ""
 }
 
 variable "bastion_launch_template_name" {
-  description = "Bastion Launch template Name, will also be used for the ASG"
+  description = "Nome do template de launch configuration utilizado também pelo ASG"
   default     = "bastion-lt"
 }
 
 variable "bastion_ami" {
   type        = string
-  description = "The AMI that the Bastion Host will use."
+  description = "AMI utilizada pelo bastion"
   default     = ""
 }
 
 variable "elb_subnets" {
   type        = list(string)
-  description = "List of subnet were the ELB will be deployed"
+  description = "Lista de subnets onde o NLB será construído"
 }
 
 variable "auto_scaling_group_subnets" {
   type        = list(string)
-  description = "List of subnet were the Auto Scalling Group will deploy the instances"
+  description = "Lista de subnets onde o ASG irá construir as instâncias"
 }
 
 variable "associate_public_ip_address" {
@@ -82,57 +78,57 @@ variable "bastion_instance_count" {
 }
 
 variable "create_dns_record" {
-  description = "Choose if you want to create a record name for the bastion (LB). If true 'hosted_zone_id' and 'bastion_record_name' are mandatory "
+  description = "Quando true cria um DNS record para o bastion (LB). Se true 'hosted_zone_id' e 'bastion_record_name' se tornam obrigatórios"
 }
 
 variable "log_auto_clean" {
-  description = "Enable or not the lifecycle"
+  description = "Ativa ou não o lifecycle do bucket"
   default     = false
 }
 
 variable "log_standard_ia_days" {
-  description = "Number of days before moving logs to IA Storage"
+  description = "Número de dias antes de mover os logs para IA Storage"
   default     = 30
 }
 
 variable "log_glacier_days" {
-  description = "Number of days before moving logs to Glacier"
+  description = "Número de dias antes de mover os logs para o Glacier"
   default     = 60
 }
 
 variable "log_expiry_days" {
-  description = "Number of days before logs expiration"
+  description = "Número de dias antes de apagar os logs"
   default     = 90
 }
 
 variable "public_ssh_port" {
-  description = "Set the SSH port to use from desktop to the bastion"
+  description = "SSH port entre desktop e o bastion"
   default     = 22
 }
 
 variable "private_ssh_port" {
-  description = "Set the SSH port to use between the bastion and private instance"
+  description = "SSH port entre bastion e instância privada"
   default     = 22
 }
 
 variable "extra_user_data_content" {
-  description = "Additional scripting to pass to the bastion host. For example, this can include installing postgresql for the `psql` command."
+  description = "Script adicional para passar para o bastion"
   type        = string
   default     = ""
 }
 
 variable "allow_ssh_commands" {
-  description = "Allows the SSH user to execute one-off commands. Pass 'True' to enable. Warning: These commands are not logged and increase the vulnerability of the system. Use at your own discretion."
+  description = "Quando 'True' permite a execução os comandos passados via SSH"
   type        = string
   default     = ""
 }
 
 variable "bastion_iam_policy_name" {
-  description = "IAM policy name to create for granting the instance role access to the bucket"
+  description = "IAM Policy criado para o bastion ter acesso ao bucket"
   default     = "BastionHost"
 }
 
 variable "instance_type" {
-  description = "Instance size of the bastion"
+  description = "Instance type do bastion"
   default     = "t3.nano"
 }
